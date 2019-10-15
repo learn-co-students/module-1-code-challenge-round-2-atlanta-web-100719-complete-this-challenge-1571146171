7d02d6b2cd19140428d3ae2bf35212737bb58da5
Dan Reeves <you@example.com>
2019-10-15 10:37:02 -0400

    OMG

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
 
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer,movie,rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating 
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+
+    def viewer 
+        Review.all.select {|r| r.viewer == self}
+        end 
+    end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+v1 = Viewer.new("Dan")
+v2 = Viewer.new("Javi")
+v3 = Viewer.new("Kuran")
+v4 = Viewer.new("Clark")
 
+m1 = Movie.new("Get Out")
+m2 = Movie.new("Transformers")
+m3 = Movie.new("Avengers: Endgame")
+m4 = Movie.new("Malibu's Most Wanted")
+m5 = Movie.new("The Lion King")
 
+r1 = Review.new(v1, m2, 3)
+r2 = Review.new(v1, m1, 5)
+r3 = Review.new(v2, m3, 2)
+r4 = Review.new(v3, m3, 4)
+r5 = Review.new(v4, m4, 5)
+r6 = Review.new(v3, m5, 2)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
